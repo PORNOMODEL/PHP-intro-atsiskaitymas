d4707874797962fd6deadf46d9522bd99f588ea9
PORNOMODEL <edvinaspranys@gmail.com>
2023-01-19 21:53:20 +0200

    Initial commit

--- praktikos_atsiskaitymas.php
+++ praktikos_atsiskaitymas.php
@@ -1,3 +1,134 @@
 <?php
 
-echo "hello";
+//1.  Grąžinkite visų lyginių skaičių, esančių $numbers masyve, sumą (1 balas)
+
+$numbers = [15, 55, 66, 91, 100, 995, 17, 550];
+$evenSum = 0;
+foreach ($numbers as $number) {
+    if ($number % 2 == 0) {
+        $evenSum += $number;
+    }
+}
+echo $evenSum . PHP_EOL;
+
+//2. Grąžinkite visų skaičių, esančių $numbers masyve, sandaugą (1 balas)
+
+$numbers = [
+    [1, 3, 5],
+    [55, 87, 100],
+    [12],
+    [],
+];
+$result = 1;
+foreach ($numbers as $subArr) {
+    foreach ($subArr as $number) {
+        $result *= $number;
+    }
+}
+echo $result . PHP_EOL;
+
+//3.     Masyve $holidays turime kelionių agentūros siūlomas keliones su kaina ir dalyvių skaičiumi.
+//Terminale išspausdinkite santrauką, kurioje matytusi miesto pavadinimas, kelionių pavadinimai ir dalyvių sumokėta suma
+//      Dėmesio! Santraukoje nerodykite tų kelionių, kurių kaina yra null. (3 balai)
+//
+//      Destination "Paris".
+//Titles: "Romantic Paris", "Hidden Paris"
+//      Total: 99500
+//************
+//      Destination "New York"
+//      ...
+//
+
+   $holidays = [
+       [
+           'title' => 'Romantic Paris',
+           'destination' => 'Paris',
+           'price' => 1500,
+           'tourists' => 55,
+       ],
+       [
+           'title' => 'Amazing New York',
+           'destination' => 'New York',
+           'price' => 2699,
+           'tourists' => 21,
+       ],
+       [
+           'title' => 'Spectacular Sydney',
+           'destination' => 'Sydney',
+           'price' => 4130,
+           'tourists' => 9,
+       ],
+       [
+           'title' => 'Hidden Paris',
+           'destination' => 'Paris',
+           'price' => 1700,
+           'tourists' => 10,
+       ],
+       [
+           'title' => 'Emperors of the past',
+           'destination' => 'Beijing',
+           'price' => null,
+           'tourists' => 10,
+       ],
+   ];
+
+$destinationTitles = [];
+$destinationTotal = [];
+foreach ($holidays as $holiday) {
+    if ($holiday['price'] == null) {
+        continue;
+    }
+    if (!isset($destinationTitles[$holiday['destination']])) {
+        $destinationTitles[$holiday['destination']] = [];
+    }
+    array_push($destinationTitles[$holiday['destination']], $holiday['title']);
+    if (!isset($destinationTotal[$holiday['destination']])) {
+        $destinationTotal[$holiday['destination']] = 0;
+    }
+    $destinationTotal[$holiday['destination']] += $holiday['price'] * $holiday['tourists'];
+}
+
+foreach ($destinationTitles as $destination => $titles) {
+    echo "Destination: \"$destination" . PHP_EOL;
+    echo "Titles: " . implode(', ', $titles) . PHP_EOL;
+    echo "Total: " . $destinationTotal[$destination] . PHP_EOL;
+    echo "************" . PHP_EOL;
+}
+
+//4. Pakoreguokite 3 užduotį taip, kad ji duomenis rašytų ne į terminalą, o spausdintų į failą. (1.5 balas)
+
+$file = fopen("holidays.txt", "w") or die("Unable to open file!");
+foreach ($destinationTitles as $destination => $titles) {
+    $txt = "Destination: \"$destination\"" . PHP_EOL .
+        "Titles: " . implode(', ', $titles) . PHP_EOL .
+        "Total: " . $destinationTotal[$destination] . PHP_EOL .
+        "************" . PHP_EOL;
+    fwrite($file, $txt);
+}
+fclose($file) . PHP_EOL;
+
+//5. Parašykite programą, kuri Jūsų susigalvotus duomenis paimtų iš failo ir atspausdintų terminale. (1.5 balas)
+
+$file = fopen("inventWords.txt", "r") or die("Unable to open file!");
+while(!feof($file)) {
+    echo fgets($file);
+}
+fclose($file) . PHP_EOL;
+
+//6. Parašykite programą, kuri per argumentų padavimą terminale paleidžiant funkciją juos priimtų ir juos sudaugintų tarpusavyje ir pakeltu kvadratu. Atkreipkite dėmesį, kad jeigu argumentas yra paduodamas ne skaičius, reikia, kad terminale gautumėme atitinkamą klaidos kodą ir pranešimą. (2 balai)
+
+$arg1 = $argv[1];
+$arg2 = $argv[2];
+function multiplyAndSquare($arg1, $arg2) {
+    if (!is_numeric($arg1) || !is_numeric($arg2)) {
+        echo "Attention: Both arguments must be numbers." . PHP_EOL;
+        return 1;
+    }
+
+    $result = ($arg1 * $arg2) ** 2;
+    echo $result . PHP_EOL;
+    return 0;
+}
+
+$attention = multiplyAndSquare($arg1, $arg2);
+
---
+++ teorijos_atsiskaitymas.php
@@ -0,0 +1,96 @@
+<?php
+
+//1.	Parašykite 1 būdą, kaip įvykdyti (paleisti) PHP script'ą iš komandinės eilutės.
+
+php -f myscript.php
+
+//2.	Kas privalo būtų PHP failo pirmoje eilurėje, kad PHP žinotų, jog tolesnės eilutės yra PHP kodas?
+
+<?php
+
+//3.	Kokiais simboliais gali prasidėti PHP kintamojo pavadinimas (po $)?
+
+raidė arba _
+
+//4.	Ar simbolis % gali būti naudojamas PHP kintamojo pavadinime?
+
+Negali
+
+//5.	Parašykite multiline PHP komentaro pavyzdį.
+
+/*
+multiline pavyzdys
+*/
+
+arba
+
+//antras pavyzdys
+
+//6.	Parašykite bent 4 duomenų tipus, naudojamus PHp kalboje.
+
+integer
+float
+string
+boolean
+
+//7.	Paminėkite pagrindinė enumeratyvaus masyvo (indexed array) požymį, kuriuo jis skiriasi nuo asociatyvaus masyvo.
+
+Yra automatiškai sugeneruojami sveikomis skaičių reikšmėmis (pavyzdžiui, 0, 1, 2, 3, ir t.t.)
+
+//8.	Turite kintamajį $someVariable = '15'. Pakeiskite jo tipą į 'int'.
+
+$someVariable = int($someVariable);
+
+//9.	Kokia funkcija naudojama gauti masyvo narių skaičiui?
+
+count();
+
+//10.	Kas nutinka, kai bandome sukurti masyvą, kuris turi kelis narius, kurių raktai (keys) sutampa?
+
+Vieno iš jų reikšmė bus išsaugota, o visi kiti bus pakeisti.
+
+//11.	Parašykite multidimensinio masyvo pavyzdį.
+
+$family = array(
+    array("Edvinas", "Pranys", 29, "male"),
+    array("Pinkis", "Maukaule", 3, "male"),
+    array("Mama", "Mamyte", 65, "female")
+);
+
+//12.	Parašykite enumeryvaus masyvo pavyzdį.
+
+cars = ['BMW', 'AUDI', 'MB']
+for index, value in enumerate(cars):
+    print(index, value)
+
+//13.	Koks operatorius yra naudojamas pridėti naują narį masyvui? Pateikite paprastą pavyzdį.
+
+array_push($cars, "MB");
+
+//14.	Kokia funkcija naudojama panaikinti masyvo nariui?
+
+unset();
+
+//15.	Kokia funkcija naudotumėte pašalinti nereikalingus simbolius nuo string tipo reikšmės pradžios ir pabaigos (pvz:. "-test-" -> "test")?
+
+trim();
+
+//16.	Kas laikoma superglobaliame kintamajame $_POST?
+
+HTML formos
+
+//17.	Kas laikoma superglobaliame kintamajame $_FILES?
+
+HTML formas kuriame yra informacija apie failą, tokia kaip failo pavadinimas, tipas, dydis ir klaida.
+
+//18.	Git status komanda leidžia padaryti ką?
+
+Parodo, kokie failai yra pakeisti, pridėti, bet necommitinti
+
+//19.	Git push komanda leidžia padaryti ką?
+
+ Commitinti failai yra perkeliami į repozitoriją.
+
+//20.	Kas yra regex?
+
+Regular Expressions -  yra naudojamas teksto analizei, teksto tvarkymui, teksto atpažinimui ir teksto filtravimui
